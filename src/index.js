e76936e0e04b4ff899d076778f439e58f0adaf62
Dima <dmitrijs.visnakovs@inbox.lv>
2023-06-05 14:39:54 +0300

    one char

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelServices from './services/MarvelService';
 import './style/style.scss';
-
-const marvelServices = new MarvelServices();
-
-marvelServices.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,26 +1,37 @@
-class MarvelServices {
+class MarvelService {
 
     _appiBas = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=3595a4aaff1c8071a7f09aad998e74d8';
 
-    getResource = async (url) =>  {
+    getResource = async (url) => {
         let res = await fetch(url);
-
+    
         if (!res.ok) {
-            throw new Error (`Could not fetch ${url}, status: ${res.status}`);
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._appiBas}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getharacter = (id) => {
-        return this.getResource(`${this._appiBas}${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=3595a4aaff1c8071a7f09aad998e74d8');
+        return this._transformCharacter(res.data.results[0]);
     }
 
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
 }
 
-export default MarvelServices;
+export default MarvelService;
